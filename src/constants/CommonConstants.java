66f8ec609e7ebe8cf871f7908f014c7687653eb5
Justine Elbuhel <telbuhel@gmail.com>
2025-04-08 17:42:13 -0700

    Second commit

--- src/constants/CommonConstants.java
+++ src/constants/CommonConstants.java
@@ -11,6 +11,6 @@ public class CommonConstants {
     // mysql credentials
     public static final String DB_URL = "jdbc:mysql://127.0.0.1:3306/login_schema";
     public static final String DB_USERNAME = "root";
-    public static final String DB_PASSWORD = "4152050516";
+    public static final String DB_PASSWORD = "";
     public static final String DB_USERS_TABLE_NAME = "USERS";
 }
